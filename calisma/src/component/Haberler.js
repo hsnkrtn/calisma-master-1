7898908f6e8ff50f7503b8400b7f757638547c50
hsnkrtn <kurtinihasan@gmail.com>
2022-02-07 16:46:19 +0300

    1

--- calisma/src/component/Haberler.js
+++ calisma/src/component/Haberler.js
@@ -1,52 +1,40 @@
 import DataConsumer from "../context";
 import { Component } from "react";
 
-
-
 class Haberler extends Component {
-static contextType = DataConsumer;
+  static contextType = DataConsumer;
   constructor(props) {
     super(props);
     this.arttirHaberId = this.arttirHaberId.bind(this);
     this.state = {
-      count: 5
+      count: 5,
     };
   }
 
- arttirHaberId (e){
-this.setState({
-    count : this.state.count+1
-
+  arttirHaberId(e) {
+    this.setState({
+      count: this.state.count + 1,
+    });
   }
 
-);
-
-}
-
   render() {
-    const { ArttirID}=this.context;
-    const HaberCount= this.state.count;
-
+    const { ArttirID } = this.context;
+    const HaberCount = this.state.count;
 
     return (
       <DataConsumer>
         {(value) => {
+          let ghabersayisi = value.ghabersayisi;
 
+          let GosterilecekHaberler = value.Haberler.filter(
+            (hbr,index) => index >= ghabersayisi
+          );
 
-//  context api içindeki enbüyükId değerini alıyor
-          let ebID= value.HaberlerSonId;
- //  context apiçindeki bütün haberleri alıyor 
+          let GosterilecekHaberlerson = GosterilecekHaberler.reverse();
+          console.log(GosterilecekHaberlerson);
 
-           let hasHaber  = value.Haberler;
-  // context api içindeki haberleri Id si ebID değerinden daha büyük olanları alıyor         
-        let gelenhaberler = hasHaber.filter(  hbr=> hbr.HaberId >=ebID );
-// gelen haberleri tersten sıralıyor
-     let   GostelrilecekHaberler= gelenhaberler.reverse();
-console.log ( ebID );
-
-        console.log ( GostelrilecekHaberler) ;
-         
-          return  <div className="HVEbaslik HVEbg ">
+          return (
+            <div className="HVEbaslik HVEbg ">
               <a className=" buyukbaslik"> Haberler & Etkinlikler </a>
               <div className="HaberlerVeEtkinlikler">
                 <div className="HLeftButton">
@@ -58,41 +46,32 @@ console.log ( ebID );
                     </a>
                   </section>
                 </div>
-                
-         {/* { hasHaber.Haberler.map((GostelrilecekHaberler) => ( */}
-         {  GostelrilecekHaberler.map (( GostelrilecekHaberler )  => 
-               <div className="Haber" id= {GostelrilecekHaberler.HaberId}
-               >
-                  <div>
-                    <img className="Hfotograf" src={GostelrilecekHaberler.HaberFotografi}></img>
-                    <span className="Htarih">
-                      <p1> Tarih </p1>
-                    </span>
+
+                {/* { hasHaber.Haberler.map((GostelrilecekHaberler) => ( */}
+                {GosterilecekHaberlerson.map((GosterilecekHaberlerson) => (
+                  <div className="Haber" id={GosterilecekHaberlerson.HaberId}>
+                    <div>
+                      <img
+                        className="Hfotograf"
+                        src={GosterilecekHaberlerson.HaberFotografi}
+                      ></img>
+                      <span className="Htarih">
+                        <p1> Tarih </p1>
+                      </span>
+                    </div>
+
+                    <a>
+                      <h1 className="HBaslik">
+                        {GosterilecekHaberlerson.HaberBasligi}
+                      </h1>
+                    </a>
+                    <p1 className="Hdetay">
+                      {GosterilecekHaberlerson.HaberDetayi}
+                    </p1>
                   </div>
+                ))}
 
-                  <a>
-                    <h1 className="HBaslik">
-                    {GostelrilecekHaberler.HaberBasligi}
-                    </h1>
-                  </a>
-                  <p1 className="Hdetay">
-                  {GostelrilecekHaberler.HaberDetayi}
-                  </p1>
-                </div>
-                )
-        }
-        
-       
-              
-              
-       
-              
-              
-
-         
-             
-
-                <div className="HRightButton" onClick={ ArttirID } >
+                <div className="HRightButton" onClick={ArttirID}>
                   <a>
                     <span>
                       <i className="fa fa-angle-right"></i>
@@ -101,8 +80,8 @@ console.log ( ebID );
                 </div>
               </div>
             </div>
-        }
-        }
+          );
+        }}
       </DataConsumer>
     );
   }
--- calisma/src/context.js
+++ calisma/src/context.js
@@ -1,125 +1,115 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-const DataContext = React.createContext ();
+const DataContext = React.createContext();
 // provider , consumeer
- export  class ContextProvider  extends Component {
+export class ContextProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+hbrDiziboyutu:null,
+ghabersayisi:null,
 
 
-    constructor(props) {
-        super(props);
-    this.state   = {
-    HaberlerSonId:5 ,
-  
+      Haberler: [
+        {
+          HaberId: 1,
+          HaberFotografi: " bga1.jpg",
+          HaberBasligi:
+            "Firat Universitesi Hastanesi1 haberleri burada gösterilecek",
+          HaberDetayi: " detay1",
+        },
+        {
+          HaberId: 2,
+          HaberFotografi: " bga5.jpg",
+          HaberBasligi:
+            "Firat Universitesi Hastanesi haberleri burada gösterilecekFirat Universitesi Hastanesi2",
+          HaberDetayi: "detay2",
+        },
+        {
+          HaberId: 3,
+          HaberFotografi: " bga2.jpg",
+          HaberBasligi:
+            " Firat Universitesi Hastaneshaberleri burada gösterilecek Firat Universitesi Hastanesi3",
+          HaberDetayi: " detay3",
+        },
+        {
+          HaberId: 4,
+          HaberFotografi: " bga2.jpg",
+          HaberBasligi:
+            " Firat Universitesi Hastaneshaberleri burada gösterilecek Firat Universitesi Hastanesi3",
+          HaberDetayi: " detay4 ",
+        },
+        {
+          HaberId: 5,
+          HaberFotografi: " bga2.jpg",
+          HaberBasligi:
+            " Firat Universitesi Hastaneshaberleri burada gösterilecek Firat Universitesi Hastanesi3",
+          HaberDetayi: " detay5 ",
+        },
+        {
+          HaberId: 6,
+          HaberFotografi: " bga2.jpg",
+          HaberBasligi:
+            " Firat Universitesi Hastaneshaberleri burada gösterilecek Firat Universitesi Hastanesi3",
+          HaberDetayi: " detay6 ",
+        },
+        {
+          HaberId: 7,
+          HaberFotografi: " bga2.jpg",
+          HaberBasligi:
+            " Firat Universitesi Hastaneshaberleri burada gösterilecek Firat Universitesi Hastanesi3",
+          HaberDetayi: " detay7 ",
+        },
+        {
+          HaberId: 8,
+          HaberFotografi: " bga2.jpg",
+          HaberBasligi:
+            " Firat Universitesi Hastaneshaberleri burada gösterilecek Firat Universitesi Hastanesi3",
+          HaberDetayi: " detay8 ",
+        },
+        {
+          HaberId: 9,
+          HaberFotografi: " bga2.jpg",
+          HaberBasligi:
+            " Firat Universitesi Hastaneshaberleri burada gösterilecek Firat Universitesi Hastanesi3",
+          HaberDetayi: " detay9 ",
+        },
+        {
+          HaberId: 10,
+          HaberFotografi: " bga2.jpg",
+          HaberBasligi:
+            " Firat Universitesi Hastaneshaberleri burada gösterilecek Firat Universitesi Hastanesi3",
+          HaberDetayi: " detay10 ",
+        },
+      ],
+    };
+  }
+   ArttirID  = ()=> {
+      this.setState({
+        hbrDiziboyutu:this.state.hbrDiziboyutu
+      })
+      console.log ( this.state.hbrDiziboyutu);
+  }
 
-    Haberler : [
-{
-    HaberId:1,
-HaberFotografi :   " bga1.jpg",
-HaberBasligi : "Firat Universitesi Hastanesi1 haberleri burada gösterilecek",
-HaberDetayi : " detay1"
+  componentDidMount() {
+
+    console.log(this.state.hbrDiziboyutu);
+    console.log(this.state.ghabersayisi);
+  }
+
+  render() {
+const Haberler =this.state.Haberler;
+this.state.hbrDiziboyutu =Haberler.length;
+this.state.ghabersayisi= this.state.hbrDiziboyutu-3;
 
 
 
-},
-{
-    HaberId:2,
-HaberFotografi :   " bga5.jpg",
-HaberBasligi : "Firat Universitesi Hastanesi haberleri burada gösterilecekFirat Universitesi Hastanesi2",
-HaberDetayi : "detay2"
-
-
-
-},
-{
-    HaberId:3,
-HaberFotografi :   " bga2.jpg",
-HaberBasligi : " Firat Universitesi Hastaneshaberleri burada gösterilecek Firat Universitesi Hastanesi3",
-HaberDetayi : " detay3"
-
-
-
-},
-{
-    HaberId:4,
-HaberFotografi :   " bga2.jpg",
-HaberBasligi : " Firat Universitesi Hastaneshaberleri burada gösterilecek Firat Universitesi Hastanesi3",
-HaberDetayi : " detay4 "
-
-
-
-},
-{
-    HaberId:5,
-HaberFotografi :   " bga2.jpg",
-HaberBasligi : " Firat Universitesi Hastaneshaberleri burada gösterilecek Firat Universitesi Hastanesi3",
-HaberDetayi : " detay5 "
-
-
-
-},
-{
-    HaberId:6,
-HaberFotografi :   " bga2.jpg",
-HaberBasligi : " Firat Universitesi Hastaneshaberleri burada gösterilecek Firat Universitesi Hastanesi3",
-HaberDetayi : " detay6 "
-
-
-
-},
-{
-    HaberId:7,
-HaberFotografi :   " bga9.jpg",
-HaberBasligi : " Firat Universitesi Hastaneshaberleri burada gösterilecek Firat Universitesi Hastanesi3",
-HaberDetayi : " detay7 "
-
-
-
-},
- ]
-};
+    return (
+      <DataContext.Provider value={{ ...this.state, ArttirID: this.ArttirID }}>
+        {this.props.children}
+      </DataContext.Provider>
+    );
+  }
 }
-// ArttirID  = ()=> {
-//     this.setState({
-//         HaberlerSonId:3
-//     })
-//     console.log ( this.state.HaberlerSonId);
-// }
-
-componentDidMount (){
-this.state.Haberler.map((IdAlacak,index) =>     
-{
-    if(IdAlacak.HaberId > this.state.HaberlerSonId){
-        this.setState({
-            HaberlerSonId:IdAlacak.HaberId
-            
-        })
-console.log (this.state.HaberlerSonId)
-    }
-
-}
-
-);
-
-}
-
-render() {
-
-  
-console.log(this.state.Haberler[0].HaberId);
-
-
-        return (
-        <DataContext.Provider value={{...this.state, ArttirID:this.ArttirID}}>
-            {this.props.children}
-
-
-          
-        </DataContext.Provider>
-        
-        )
-     
-
-    }
-}
-const DataConsumer= DataContext.Consumer;
-export default DataConsumer ;
+const DataConsumer = DataContext.Consumer;
+export default DataConsumer;
