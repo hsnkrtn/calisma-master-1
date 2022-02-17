82df9446256454c75f3932cce7b73364096ccdd4
hsnkrtn <kurtinihasan@gmail.com>
2022-02-17 11:31:26 +0300

    1

--- calisma/src/component/Helpers/CarouselData.js
+++ calisma/src/component/Helpers/CarouselData.js
@@ -1,6 +1,7 @@
-    import hastane1 from "../Resimler/hastane1.jpg"
+import hastane1 from "../Resimler/hastane1.jpg"
 import hastane2 from "../Resimler/hastane2.jpg"
 import hastane3 from "../Resimler/hastane3.jpg"
+import hastane4 from "../Resimler/hastane4.jpg"
 
 export const  images = [
     {
@@ -16,6 +17,12 @@ export const  images = [
     {
         title: " hasankrtn2",
         a: hastane3,
+
+    },
+
+    {
+        title: " hasankrtn2",
+        a: hastane4,
 
     },
 
