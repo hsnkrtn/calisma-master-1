b596d760be4defa73fa4df28dedbfae98a72c553
hsnkrtn <kurtinihasan@gmail.com>
2022-02-28 13:10:24 +0300

    1

---
+++ calisma/src/component/Helpers/KisiFotografları.js
@@ -0,0 +1,116 @@
+import ayhanakbal from "../Resimler/KısıFotografları/ayhanakbal.jpg"
+import aysealbay from "../Resimler/KısıFotografları/aysealbay.jpeg"
+import azperilioglu from "../Resimler/KısıFotografları/azperilioglu.jpg"
+import bashekim from "../Resimler/KısıFotografları/bashekim.jpg"
+import birginbidav from "../Resimler/KısıFotografları/birginbidav.jpg"
+import cevatgun from "../Resimler/KısıFotografları/cevatgun.jpg"
+import enginozkaya from "../Resimler/KısıFotografları/enginozkaya.jpg"
+import fatihfirdolas from "../Resimler/KısıFotografları/fatihfirdolas.jpg"
+import feritgunes from "../Resimler/KısıFotografları/feritgunes.jpg"
+import gokhanartas from "../Resimler/KısıFotografları/gokhanartas.jpg"
+import gulizarozturk from "../Resimler/KısıFotografları/gulizarozturk.jpg"
+import ibrahimaslan from "../Resimler/KısıFotografları/ibrahimaslan.jpg"
+import ilyasturk from "../Resimler/KısıFotografları/ilyasturk.jpeg"
+import ismaildemirel from "../Resimler/KısıFotografları/ismaildemirel.jpg"
+import meltemyildiz from "../Resimler/KısıFotografları/meltemyildiz.jpeg"
+import metincebeci from "../Resimler/KısıFotografları/metincebeci.jpg"
+import muratgonen from "../Resimler/KısıFotografları/muratgonen.jpg"
+import mustafayildiz from "../Resimler/KısıFotografları/mustafayildiz.jpeg"
+import nurcanince from "../Resimler/KısıFotografları/nurcanince.png"
+import selcenozkaya from "../Resimler/KısıFotografları/selcenozkaya.jpeg"
+import talatceylan from "../Resimler/KısıFotografları/talatceylan.jpg"
+
+export const  HastaneKisiFotograflari = [
+    {
+        Fotograf: ayhanakbal,
+
+    },
+    {
+        Fotograf: aysealbay,
+
+    },
+    {
+        Fotograf: azperilioglu,
+
+    },
+    {
+        Fotograf: bashekim,
+
+    },
+    {
+        Fotograf: birginbidav,
+
+    },
+    {
+        Fotograf: cevatgun,
+
+    },
+    {
+        Fotograf: enginozkaya,
+
+    },
+    {
+        Fotograf: fatihfirdolas,
+
+    },
+    {
+        Fotograf: feritgunes,
+
+    },
+    {
+        Fotograf: gokhanartas,
+
+    },
+    {
+        Fotograf: gulizarozturk,
+
+    },
+    {
+        Fotograf: ibrahimaslan,
+
+    },
+    {
+        Fotograf: ilyasturk,
+
+    },
+    {
+        Fotograf: ismaildemirel,
+
+    },
+    {
+        Fotograf: meltemyildiz,
+
+    },
+    {
+        Fotograf: metincebeci,
+
+    },
+    {
+        Fotograf: muratgonen,
+
+    },
+    {
+        Fotograf: mustafayildiz,
+
+    },
+    {
+        Fotograf: nurcanince,
+
+    },
+    {
+        Fotograf: selcenozkaya,
+
+    },
+
+    {
+        Fotograf: talatceylan,
+
+    },
+
+   
+
+] ;
+export default {
+    HastaneKisiFotograflari,
+}
+
--- calisma/src/component/Yonetim.js
+++ calisma/src/component/Yonetim.js
@@ -13,6 +13,7 @@ function Yonetim(props) {
         className="RouterYonetimSayfasiIcerik
 "
       >
+        <h1> BASLIK </h1>
         <ul className="YonetimCards">
           {Gelenveriler.map((Gelenveriler, index) => {
             return (
@@ -20,6 +21,7 @@ function Yonetim(props) {
                 <img className="YonetimFotograf" src={fotograf}></img>
                 <section className="YonetimDetay">
                   <h1> Hasan</h1>
+                  <h2> Hasan</h2>
                   <p1> 1976 yılında Elazığ’ da doğdu. İlk, orta ve lise eğitimini Elazığ’ da tamamladı.2000 yılında Fırat Üniversitesi Tıp Fakültesinden mezun oldu.2000-2002 Van Askeri Hastanesinde Yedek Subay olarak görev yaptı.2002-2008 yılları arasında Fırat Üniversitesi Üroloji Anabilim Dalında uzmanlık eğitimini tamamladı. 2008-2009 yılları arasında Diyarbakır Bismil Devlet Hastanesinde mecburi hizmetini tamamladı.2009 yılında Fırat Üniversitesi Üroloji Anabilim Dalına Yardımcı Doçent olarak atandı. 2014 yılında Doçent unvanını, 2019 yılında ise Profesör unvanını aldı. 2015 yılından itibaren Fırat Üniversitesi Hastanesi Başhekim Yardımcısı olarak görevlendirildi. 2020 yılı içerisinde Fırat Üniversitesi Tıp Fakültesi Dekan Yardımcısı olarak görev yaptı. Ulusal ve uluslararası makale ve bildirileri bulunmakta olup evli ve iki çocuk babasıdır. </p1>
                 </section>
               </li>
