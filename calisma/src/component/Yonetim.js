77bbe8054e3afd2ce69fc76905c11fad1ce3f8d7
hsnkrtn <kurtinihasan@gmail.com>
2022-02-28 11:07:46 +0300

    1

--- calisma/src/component/Yonetim.js
+++ calisma/src/component/Yonetim.js
@@ -9,14 +9,19 @@ function Yonetim(props) {
 
   return (
     <div className="RouterSayfasi">
-      <div className="RouterSayfasiIcerik">
+      <div
+        className="RouterYonetimSayfasiIcerik
+"
+      >
         <ul className="YonetimCards">
           {Gelenveriler.map((Gelenveriler, index) => {
             return (
               <li className="YonetimCard">
                 <img className="YonetimFotograf" src={fotograf}></img>
-                <h1> Hasan</h1>
-                <p1> Kurtini </p1>
+                <section className="YonetimDetay">
+                  <h1> Hasan</h1>
+                  <p1> 1976 yılında Elazığ’ da doğdu. İlk, orta ve lise eğitimini Elazığ’ da tamamladı.2000 yılında Fırat Üniversitesi Tıp Fakültesinden mezun oldu.2000-2002 Van Askeri Hastanesinde Yedek Subay olarak görev yaptı.2002-2008 yılları arasında Fırat Üniversitesi Üroloji Anabilim Dalında uzmanlık eğitimini tamamladı. 2008-2009 yılları arasında Diyarbakır Bismil Devlet Hastanesinde mecburi hizmetini tamamladı.2009 yılında Fırat Üniversitesi Üroloji Anabilim Dalına Yardımcı Doçent olarak atandı. 2014 yılında Doçent unvanını, 2019 yılında ise Profesör unvanını aldı. 2015 yılından itibaren Fırat Üniversitesi Hastanesi Başhekim Yardımcısı olarak görevlendirildi. 2020 yılı içerisinde Fırat Üniversitesi Tıp Fakültesi Dekan Yardımcısı olarak görev yaptı. Ulusal ve uluslararası makale ve bildirileri bulunmakta olup evli ve iki çocuk babasıdır. </p1>
+                </section>
               </li>
             );
           })}
