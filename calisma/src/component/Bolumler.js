4ff1259d1e68f8a51863ff98ffd6079be96d3d9f
hsnkrtn <kurtinihasan@gmail.com>
2022-03-02 16:58:57 +0300

    1

--- calisma/src/component/Bolumler.js
+++ calisma/src/component/Bolumler.js
@@ -6,18 +6,31 @@ function Bolumler(props) {
   const fotograflar = HastaneFotograflari[5].a;
 
   const location = useLocation();
-  const Gonderilenveriler = location.state.Gonderilenveriler;
-const Baslik=  Gonderilenveriler.BolumAdi;
+  const Gelenveriler = location.state.Gonderilenveriler;
+  const Baslik = Gelenveriler.BolumAdi;
+  const Detay = Gelenveriler.Detay;
+  const Doktorlar = Gelenveriler.Doktorlar;
+
+
+  console.log(Doktorlar);
+
   return (
     <div className="RouterSayfasi">
       <div className="RouterSayfasiIcerik">
         <img className="RouterSayfasiFotograf" src={fotograflar}></img>
-        <section  className="RouterSayfasiDetay" > 
+        <section className="RouterSayfasiDetay">
           <h1>{Baslik}</h1>
-          <hr></hr>
-          <p1> </p1>
+          <p1>{Detay} </p1>
         </section>
-        
+        <div className="BolumlerDoktorlar">
+
+            
+
+
+
+
+
+        </div>
       </div>
     </div>
   );
